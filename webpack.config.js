086fd9dc025eb3316b72a818eb1a49b0480f0ade
ZiQiangWang <814120507@qq.com>
2017-07-04 18:59:07 +0800

    Implement zip function of python done

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2017 ZiQiangWang
+ * License: MIT
+ * GitHub: https://github.com/ZiQiangWang/js-zip
+ **/
+
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './src/js-zip.js',
+  output: {
+    filename: 'js-zip.min.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader:'babel-loader',
+        exclude: path.resolve(__dirname, 'node_modules'),
+        include: path.resolve(__dirname, 'src'),
+      },
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      output: { comments: false },
+      compress: { warnings: false }
+    })
+  ]
+}
+
+
